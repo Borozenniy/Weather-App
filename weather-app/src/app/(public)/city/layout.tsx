4cad7e3386a107db6754d5a2eecd7baf5a7e6661
Serhii Borozennyi <106269522+Borozenniy@users.noreply.github.com>
2025-11-13 19:14:08 +0100

    [fix] styles

--- weather-app/src/app/(public)/city/layout.tsx
+++ weather-app/src/app/(public)/city/layout.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from 'react';
 
 function Layout({ children }: PropsWithChildren) {
   return (
-    <div className='w-full min-h-screen flex items-center justify-start pt-28 bg-gray-50 dark:bg-gray-900 dark:text-zinc-50 '>
+    <div className='w-full min-h-screen flex items-center justify-center pt-28 bg-gray-50 dark:bg-gray-900 dark:text-zinc-50 '>
       {children}
     </div>
   );
